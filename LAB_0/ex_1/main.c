fb1db0ccf18ce74862fa32c46e2a3b20b3d8c9fc
Alessandro Gibello <a.gibello03@gmail.com>
2024-10-19 20:19:59 +0200

    updates

---
+++ LAB_0/ex_1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    char row[100+1], word[20+1];
+    // memorize a line and put it in an array
+
+    //
+
+    printf("Hello, World!\n");
+    return 0;
+}
